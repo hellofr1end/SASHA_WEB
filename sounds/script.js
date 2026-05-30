a55f683bbfaa86222bddf3c32625479928c48be4
hellofr1end <64679306+hellofr1end@users.noreply.github.com>
2026-05-30 19:04:50 +0700

    Update script.js

--- sounds/script.js
+++ sounds/script.js
@@ -1,1 +1,17 @@
 
+
+
+let dexterSound = new Audio('dexter-meme.mp3')
+let bush = document.querySelector('.bush')
+let negr = document.querySelector('.negr')
+
+bush.addEventListener('mouseover', function(){
+    negr.classList.add('active')
+    dexterSound.play();
+})
+
+bush.addEventListener('mouseout', function(){
+    negr.classList.remove('active')
+    dexterSound.pause();
+    dexterSound.currentTime = 0;
+})
