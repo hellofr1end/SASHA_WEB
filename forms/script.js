597d3a15d05b50b11c8ecc226851e38d47b7d395
hellofr1end <64679306+hellofr1end@users.noreply.github.com>
2026-06-13 19:55:08 +0700

    Update script.js

--- forms/script.js
+++ forms/script.js
@@ -1,1 +1,14 @@
 
+let form = document.querySelector('.regForm')
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let data = new FormData(form);
+
+    console.log('никнейм:', data.get('nickname'));
+    console.log('фио:', data.get('fio'));
+    console.log('пароль:', data.get('pass'));
+
+})
